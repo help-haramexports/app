560586bac39d79c8cf5fc6c853b3fe5c16610360
help-haramexports <help.haramexports@gmail.com>
2026-04-25 15:56:56 +0530

    Fixed collection page

--- app/collections/[handle]/page.tsx
+++ app/collections/[handle]/page.tsx
@@ -1,6 +1,6 @@
 export default function CollectionPage() {
   return (
-    <div className="p-20 text-center text-xl">
+    <div className="p-20 text-center text-xl font-bold mt-20">
       Collection Page Coming Soon...
     </div>
   );
